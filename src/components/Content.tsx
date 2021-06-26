c2883a4179e472ff93d24a0b0958c8841467fc41
Caique Ortega <cortegamdx@gmail.com>
2021-06-26 11:11:44 -0300

    Desafio finalizado

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,52 @@
-export function Content() {
-  // Complete aqui
+import { useEffect, useState } from "react";
+import { api } from "../services/api";
+import { MovieCard } from "./MovieCard";
+import "../styles/content.scss";
+
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+  
+}
+
+interface GenreProps{
+  id:number
+  title:string;
+}
+
+export function Content(props:GenreProps) {
+
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+
+  useEffect(() =>{
+    
+    api.get<MovieProps[]>(`movies/?Genre_id=${props.id}`).then(response => {
+      setMovies(response.data);
+    });
+  },[props.id])
+  
+  return (
+   
+      <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {props.title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+  )
+
 }
